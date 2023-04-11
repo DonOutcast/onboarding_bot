4125c8d2e46d45c0e6820b52c66b813eb5dff871
DonOutcast <lymondgl@student.21-school.ru>
2023-04-11 22:26:47 +0300

    without containres

--- src/onboarding/core/apps.py
+++ src/onboarding/core/apps.py
@@ -1,6 +1,7 @@
 from django.apps import AppConfig
 
 
-class AdminConfig(AppConfig):
+class CoreConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'admin'
+    name = 'core'
+
