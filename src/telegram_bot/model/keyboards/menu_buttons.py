fad8947184cd2c86d4374c69bdc117a9ca71504f
macbook <ikamil522@gmail.com>
2023-04-11 13:58:15 +0300

    созданние меню

--- src/telegram_bot/model/keyboards/menu_buttons.py
+++ src/telegram_bot/model/keyboards/menu_buttons.py
@@ -1,4 +1,38 @@
-from aiogram.types.keyboard_button import KeyboardButton
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 
+# menu_buttons = [
+#     [
+#         InlineKeyboardButton(callback_data='Study', text='Обучение'),
+#         InlineKeyboardButton(callback_data='company', text='О нас')
+#     ],
+#     [
+#         InlineKeyboardButton(callback_data='faq', text='Задать вопрос'),
+#         InlineKeyboardButton(callback_data='map', text='Карта')
+#     ],
+#     [
+#         InlineKeyboardButton(callback_data='employees', text='Сотрудники'),
+#         InlineKeyboardButton(callback_data='unknown', text='не знаю')
+#     ]
+# ]
 
+# menu_keyboard = InlineKeyboardMarkup(inline_keyboard=menu_buttons)
 
+menu_buttons = [
+    [
+        KeyboardButton(text='Обучение'),
+        KeyboardButton(text='Карта')
+    ],
+    [
+        KeyboardButton(text='О нас'),
+        KeyboardButton(text='Наши сотрудники')
+    ],
+    [
+        KeyboardButton(text='Задать вопрос'),
+        KeyboardButton(text='О себе')
+    ],
+    [KeyboardButton(text='Админка')]
+
+]
+
+menu_keyboard = ReplyKeyboardMarkup(
+    keyboard=menu_buttons, resize_keyboard=True)
